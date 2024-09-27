7bc9cbc3d331b975288442844f7e5236498afe71
Akronixx <josectp12yt@gmail.com>
2024-09-26 19:12:52 -0500

    Parte 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int aciertoN=0,acierto=0,pd=0,sd=0,td=0,ele, num = rnd.Next(1, 37);
+            int aciertoN=0,acierto=0,pd=0,sd=0,td=0,ele=-1,docena=0, num = rnd.Next(1, 37);
             string[] colores = { "ROJO", "NEGRO" };
             string op;
             Random random = new Random();
@@ -31,59 +31,117 @@ namespace ConsoleApp1
             if(op=="S")
             {
                 aciertoN = aciertoN+1;
+                Console.WriteLine("Elija un número (1-36):");
+                ele = int.Parse(Console.ReadLine());
+                while (ele < 0 || ele > 36)
+                {
+                    Console.WriteLine("Ingresé una opción del 1 al 36");
+                    ele = int.Parse(Console.ReadLine());
+                }
+                if (ele == num)
+                {
+                    acierto = acierto + 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Le tienes miedo al exito");
             }
             //numero
-            Console.WriteLine("Elija un número (1-36):");
-            ele=int.Parse(Console.ReadLine());
-            while (ele<0 || ele>36)
+            Console.WriteLine("¿Desea elegir un color?(S/N)");
+            op = Console.ReadLine().ToUpper();
+            while (op != "S" && op != "N")
             {
-                Console.WriteLine("Ingresé una opción del 1 al 36");
-                ele = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresé una opción valida (S/N)");
+                op = Console.ReadLine().ToUpper();
+            }
+            if (op == "S")
+            {
+                aciertoN = aciertoN + 1;
+                Console.WriteLine("Elija un color (Rojo-Negro):");
+                colorele = Console.ReadLine().ToUpper(); ;
+                while (colorele != "ROJO" && colorele != "NEGRO")
+                {
+                    Console.WriteLine("Elija un color valido (Rojo-Negro):");
+                    colorele = Console.ReadLine().ToUpper(); ;
+                }
+                if (colorele == colorAleatorio)
+                {
+                    acierto = acierto + 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Estas perdiendo plata crack");
             }
             //color
-            Console.WriteLine("Elija un color (Rojo-Negro):");
-            colorele = Console.ReadLine().ToUpper(); ;
-            while (colorele != "ROJO" && colorele != "NEGRO")
+            Console.WriteLine("¿Desea elegir incluir la docena?(S/N)");
+            op = Console.ReadLine().ToUpper();
+            while (op != "S" && op != "N")
             {
-                Console.WriteLine("Elija un color valido (Rojo-Negro):");
-                colorele = Console.ReadLine().ToUpper(); ;
+                Console.WriteLine("Ingresé una opción valida (S/N)");
+                op = Console.ReadLine().ToUpper();
             }
-            if(colorele==colorAleatorio)
+            if (op == "S")
             {
-                acierto = acierto + 1;
+                aciertoN = aciertoN + 1;
+                Console.WriteLine("Elija una docena (1-2-3):");
+                docena = int.Parse(Console.ReadLine());
+                while (docena !=1 && docena != 2 && docena!=3)
+                {
+                    Console.WriteLine("Elija una docena valida (1-2-3):");
+                    docena = int.Parse(Console.ReadLine());
+                }
+                if (num  >= 0 && num < 13)
+                {
+                    Console.WriteLine("Primera docena");
+                    pd = pd + 1;
+                }
+                else if (num > 12 && num < 25)
+                {
+                    Console.WriteLine("Segunda docena");
+                    sd = sd + 1;
+                }
+                else if (num > 24 && num < 37)
+                {
+                    Console.WriteLine("Tercera docena");
+                    td = td + 1;
+                }
+                switch(docena)
+                {
+                    case 1:
+                        if (num >= 0 && num < 13)
+                        {
+                            Console.WriteLine("Primera docena");
+                            pd = pd + 1;
+                        }
+                        break;
+                    case 2:
+                        if (num > 12 && num < 25)
+                        {
+                            Console.WriteLine("Segunda docena");
+                            sd = sd + 1;
+                        }
+                        break;
+                    case 3:
+                        if (num > 24 && num < 37)
+                        {
+                            Console.WriteLine("Tercera docena");
+                            td = td + 1;
+                        }
+                        break;
+                }
+                if (pd == 2 || sd == 2 || td == 2)
+                {
+                    acierto = acierto + 1;
+                }
             }
+            else
+            {
+                Console.WriteLine("Estas perdiendo plata crack");
+            }
+
             //docena
-            if (num >= 0 && num < 13)
-            {
-                Console.WriteLine("Primera docena");
-                pd = pd + 1;
-            }
-            else if (num > 12 && num < 25)
-            {
-                Console.WriteLine("Segunda docena");
-                sd = sd + 1;
-            }
-            else if (num > 24 && num < 37)
-            {
-                Console.WriteLine("Tercera docena");
-                td = td + 1;
-            }
-            if (num >= 0 && num <= 13)
-            {
-                pd = pd + 1;
-            }
-            else if (ele >= 12 && ele <= 25)
-            {
-                sd = sd + 1;
-            }
-            else if (ele >= 24 && ele <= 37)
-            {
-                td = td + 1;
-            }
-            if(pd==2 || sd==2 || td==2)
-            {
-                acierto = acierto + 1;
-            }
             Console.WriteLine(num);
             Console.WriteLine(colorAleatorio);
             if (acierto == aciertoN)
